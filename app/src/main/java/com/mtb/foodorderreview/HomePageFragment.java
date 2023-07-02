17686b636b869101c5afd5e1daf5bce89785450a
minhquan1313 <minhquan1313@gmail.com>
2023-07-02 12:03:18 +0700

    up

--- app/src/main/java/com/mtb/foodorderreview/HomePageFragment.java
+++ app/src/main/java/com/mtb/foodorderreview/HomePageFragment.java
@@ -1,12 +1,13 @@
-package com.mtb.foodorderreview;
+package com.mtb.foodorderreview.homeview;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.mtb.foodorderreview.R;
 
 /**
  * A simple {@link Fragment} subclass.
