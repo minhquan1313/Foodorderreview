2bf1f26986f8afaf71d5e074ebade8cef5c0860f
minhquan1313 <minhquan1313@gmail.com>
2023-07-20 19:38:29 +0700

    up

--- app/src/main/java/com/mtb/foodorderreview/HomePageFragment.java
+++ app/src/main/java/com/mtb/foodorderreview/HomePageFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -21,7 +22,6 @@ import com.mtb.foodorderreview.homeview.FoodTypeGridAdapter;
 import com.mtb.foodorderreview.homeview.Restaurant;
 import com.mtb.foodorderreview.homeview.RestaurantRecyclerAdapter;
 import com.mtb.foodorderreview.service.FoodCategoryType;
-import com.mtb.foodorderreview.utils.Utils;
 
 import java.util.Arrays;
 
@@ -34,8 +34,10 @@ public class HomePageFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    TextView home_page_user_name;
+    TextView home_page_user_name,
+            restaurant_cart_quantity_text;
     ImageView home_user_avatar1;
+    RelativeLayout home_shipping_btn;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -92,8 +94,8 @@ public class HomePageFragment extends Fragment {
 
         home_page_user_name.setText(UserGlobal.getInstance().getName());
 
-        String url = "https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg";
-        Utils.UI.setSrc(url, home_user_avatar1);
+//        String url = "https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg";
+//        Utils.UI.setSrc(url, home_user_avatar1);
     }
 
     private void HomeFoodShopUI(Context context, View view) {
